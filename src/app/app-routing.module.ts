3af119c78b95d7c793fc7f559c8e86d4183656d8
Frederico Oliveira <freddie.oliveira95@gmail.com>
2024-03-01 16:18:02 +0000

    Angular Router - #17

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
+import { EmitterComponent } from './components/emitter/emitter.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: FirstComponentComponent
+  },
+  {
+    path: 'list', component: ListRenderComponent
+  },
+  {
+    path: 'if', component: IfRenderComponent
+  },
+  {
+    path: 'event', component: EmitterComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
